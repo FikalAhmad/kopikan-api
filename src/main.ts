75f5c64f3567d73a4aafa5c25c300b69ecb5826e
FikalAhmad <fikaalm26@gmail.com>
2025-02-16 19:28:44 +0700

    minor updates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { logger } from "./application/logging";
 import { web } from "./application/web";
 
-web.listen(5000, () => {
-  logger.info("Listening on port 5000");
+web.listen(4000, () => {
+  logger.info("Listening on port 4000");
 });
