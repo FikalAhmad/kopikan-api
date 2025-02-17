c321c703d0639b82926edf588301913268de22c0
FikalAhmad <fikaalm26@gmail.com>
2025-02-17 09:54:53 +0700

    cors updated

--- src/application/web.ts
+++ src/application/web.ts
@@ -10,9 +10,7 @@ export const web = express();
 web.use(express.json());
 web.use(
   cors({
-    origin: (origin, callback) => {
-      callback(null, true);
-    },
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
