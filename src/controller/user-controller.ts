515490939793326cd8b9b75354a49605766a113f
FikalAhmad <fikaalm26@gmail.com>
2025-02-17 16:42:47 +0700

    test domain

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -98,6 +98,8 @@ export const Login = async (req: Request, res: Response) => {
       maxAge: 24 * 60 * 60 * 1000,
       sameSite: "none",
       secure: true,
+      domain:
+        process.env.NODE_ENV === "production" ? ".vercel.app" : "localhost",
     });
     // res.cookie("refreshToken", refreshToken, {
     //   httpOnly: true,
